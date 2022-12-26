2d914e4fb0a10247934a18e6fa89f1c68d09eb22
Artur Bertoni <artur_b_santos@hotmail.com>
2022-12-26 14:41:52 -0300

    Bug fixes docker-compose & app local atualizada

--- src/main/resources/insert.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO tb_product (code, category, series, description, color, material, name, gross_amount, taxes, price, quantity, bar_code, manufacturing_date, expiration_date)
-VALUES ('21g437s','FOOD','1/2022','Fruta','Vermelha','n/a','Maçã',10,10,11,150,123456789012, DATE '2022-11-20', DATE '2022-10-03');
-
-INSERT INTO tb_product (code, category, series, description, color, material, name, gross_amount, taxes, price, quantity, bar_code, manufacturing_date, expiration_date)
-VALUES ('21g438s','FOOD','1/2022','Fruta','Amarela','n/a','Banana',10,10,11,150,123456789013, DATE '2022-11-20', DATE '2022-10-03');
-
-INSERT INTO tb_product (code, category, series, description, color, material, name, gross_amount, taxes, price, quantity, bar_code, manufacturing_date, expiration_date)
-VALUES ('21g439s','FOOD','1/2022','Grão','Preto','n/a','Feijão',10,10,11,150,123456789014, DATE '2022-11-20', DATE '2022-10-03');
